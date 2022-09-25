767d0ee857dc43a4af5a8ca60c7a5c2f19b6af97
hugo-to <84351869+hugo-to@users.noreply.github.com>
2022-09-25 11:07:22 -0300

    Binarios

---
+++ Curso de Pensamiento Lógico--Algoritmos y Diagramas de Flujo/Binario-y-Decimal.js
@@ -0,0 +1,20 @@
+// 🌈 Hacks para convertir números decimales y binarios 🌈
+// …
+
+// ✨ A binarios ✨ divide el número cuantas veces se pueda entre 2
+
+// Cuando el producto sea impar será equivalente a 1
+// 30/2 = 15 ➡️ 1
+// Cuando el producto sea par será equivalente a 0
+// 20/2 = 10 ➡️ 0
+// · Cuando el número sea > 2 sabrás que ya has terminado
+// · Para leer el resultado ve desde el último número al primero (de la última división a la primera) ↖️
+
+// …
+
+// ✨ A decimales ✨ multiplica cada uno de los números x 2 y sus potencias según su posición de derecha a izquierda ↩️
+
+// · Si el número es 11100 entonces la posición 0 es el último０ -> 0 · 2^0
+// · Luego suma los resultados de las multiplicaciones
+
+
